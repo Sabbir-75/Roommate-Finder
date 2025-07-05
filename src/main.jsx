6cc38790a806f76c77275f52539a1edf61697f9f
Sabbir-75 <kazisabbir4361@gmail.com>
2025-07-05 17:25:09 +0600

    updated structure of main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import AuthProvider from './AuthProvider/AuthProvider'
+import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './Routes/Routes'
-import { Helmet } from 'react-helmet'
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
         <RouterProvider router={router}></RouterProvider>
     </AuthProvider>
-
 )
